f891c9be3503ebdbbaa7e8d4d133756c68cc1860
Александр Боровик <aoborovik.sbt@sberbank.ru>
2023-08-23 18:01:59 +0600

    start spring-boot with vaadin

---
+++ spring-boot-vaadin/src/main/java/com/unsomen/springbootvaadin/SpringBootVaadinApplication.java
@@ -0,0 +1,13 @@
+package com.unsomen.springbootvaadin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVaadinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootVaadinApplication.class, args);
+	}
+
+}
---
+++ spring-boot-vaadin/src/test/java/com/unsomen/springbootvaadin/SpringBootVaadinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unsomen.springbootvaadin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootVaadinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
